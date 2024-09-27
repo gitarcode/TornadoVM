22618810e76e301e894cd75afbc7805f27e4cb22
Gitar <noreply@gitar.co>
2024-09-27 00:28:22 +0000

    [Gitar] Updating Java files

--- tornado-unittests/src/main/java/module-info.java
+++ tornado-unittests/src/main/java/module-info.java
@@ -1,5 +1,10 @@
 open module tornado.unittests {
-  requires transitive junit;
+  requires junit;
+  requires transitive org.junit.jupiter.api;
+  requires org.junit.platform.commons;
+  requires org.junit.jupiter.engine;
+  requires org.junit.platform.launcher;
+  requires org.hamcrest;
   requires transitive tornado.api;
   requires lucene.core;
   requires java.desktop;
